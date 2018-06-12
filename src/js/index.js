9f8bfe55bdc72f8717d93c0876f930978a67ded4
Dan-Y-Ko <dk3790@hotmail.com>
2018-06-12 18:55:34 -0400

    this is hard

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,44 @@
 require('../styles/app.css')
-import num from './test';
 
-console.log(`I imported ${num} from another module!!`);
+
+function toggle()
+{
+    if (modal.style.display == 'flex')
+    {
+        modal.style.display = 'none';
+    }
+    else
+    {
+        modal.style.display = 'flex';
+    }
+};
+
+const modal = document.querySelector('.todo_modal');
+document.querySelector('.todo__container').addEventListener('click', toggle);
+
+let todo_array = [];
+const input = document.querySelector('#todo_id');
+
+const addItem = () =>
+{
+    todo_array.push(input.value);
+}
+
+const display_todo = (item) =>
+{
+    let html =
+    `
+        <div>${item}</div>
+    `;
+
+    document.querySelector('.todo_modal').insertAdjacentHTML('beforeend', html);
+}
+
+document.addEventListener('keypress', event =>
+{
+    if (event.keyCode === 13 || event.which === 13)
+    {
+        addItem();
+        display_todo(todo_array);
+    }
+});
