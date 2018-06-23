e0be9a9f8dd4f02b526d62a50b2432b74bc5e4cd
Daniel Ko <dk3790@hotmail.com>
2018-06-23 16:44:45 -0400

    updated todo

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 require('../styles/app.css')
-import num from './test';
+import uuidv4 from 'uuid/v4';
+import { renderTodos, saveTodos, loadTodos } from './todos';
 //sara:comment out following line
 // import * as setTime from './clock';
 
@@ -9,74 +10,26 @@ import { setTime } from './clock';
 // console.log(`I imported ${num} from another module!! ${clock}` );
 //sara:added following line
 setInterval(setTime, 1000);
-
-
-
-
 import _ from './weather';
+/**----------------------------------------------------------- TODOS ------------------------------------------------------- **/
+export let todos = loadTodos();
+renderTodos(todos);
 
 
-console.log(`I imported ${num} from another module!!`);
-
-function get_todos() {
-    let todos = new Array;
-    let todos_str = localStorage.getItem('todo');
-    if (todos_str !== null) {
-        todos = JSON.parse(todos_str);
-    }
-    return todos;
-}
-
-function add() {
-    var task = document.getElementById('task').value;
-
-    var todos = get_todos();
-
-    if (task != '')
+document.querySelector('#new-todo').addEventListener('keypress', function (e) {
+    // e.preventDefault()
+    
+    if (e.keyCode === 13 || e.which ===13)
     {
-        todos.push(task);
-        localStorage.setItem('todo', JSON.stringify(todos));
-    }
-
-    show();
-
-    return false;
-}
-
-function remove() {
-    var id = this.getAttribute('id');
-    var todos = get_todos();
-    todos.splice(id, 1);
-    localStorage.setItem('todo', JSON.stringify(todos));
-
-    show();
-
-    return false;
-}
-
-function show() {
-    var todos = get_todos();
-
-    var html = '<ul>';
-    for (var i = 0; i < todos.length; i++) {
-        html += '<li>' + todos[i] + '<button class="remove" id="' + i + '">x</button></li>';
-    };
-    html += '</ul>';
-
-    document.getElementById('todos').innerHTML = html;
-
-    var buttons = document.getElementsByClassName('remove');
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', remove);
-    };
-}
-
-document.getElementById('task').addEventListener('keypress', e =>
-{
-    if (e.keyCode === 13 || e.which === 13)
-    {
-        add();
-        document.getElementById('task').value = '';
+        todos.push({
+            id: uuidv4(),
+            text: e.target.value,
+            completed: false
+        })
+        saveTodos(todos);
+        renderTodos(todos);
+        e.target.value = '';
     }
 });
-show();
+
+/**----------------------------------------------------------- TODOS ------------------------------------------------------- **/
